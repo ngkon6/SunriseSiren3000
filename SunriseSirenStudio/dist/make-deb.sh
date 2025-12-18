95f5ed05f8bbc717db870fe82b88359c079eb340
ngkon6 <nathankoning@gmail.com>
2025-12-18 13:45:24 +0100

    Increase make-deb.sh verbosity

--- SunriseSirenStudio/dist/make-deb.sh
+++ SunriseSirenStudio/dist/make-deb.sh
@@ -15,18 +15,18 @@ if [ -d "$BUILD_TARGET" ]; then
     rm -rf "$BUILD_TARGET"
 fi
 
-mkdir -p "$BUILD_TARGET/DEBIAN"
-mkdir -p "$BUILD_TARGET/usr/bin"
-mkdir -p "$BUILD_TARGET/usr/share/applications"
-mkdir -p "$BUILD_TARGET/usr/share/glib-2.0/schemas"
-mkdir -p "$BUILD_TARGET/usr/share/icons/hicolor/256x256@2/apps"
-mkdir -p "$BUILD_TARGET/usr/share/man/man1"
+mkdir -vp "$BUILD_TARGET/DEBIAN"
+mkdir -vp "$BUILD_TARGET/usr/bin"
+mkdir -vp "$BUILD_TARGET/usr/share/applications"
+mkdir -vp "$BUILD_TARGET/usr/share/glib-2.0/schemas"
+mkdir -vp "$BUILD_TARGET/usr/share/icons/hicolor/256x256@2/apps"
+mkdir -vp "$BUILD_TARGET/usr/share/man/man1"
 
-cp "$DIR/../build/sunrise-siren-studio" "$BUILD_TARGET/usr/bin"
-cp "$DIR/../dist/sunrise-siren-studio.desktop" "$BUILD_TARGET/usr/share/applications"
-cp "$DIR/../dist/sunrise-siren-studio.png" "$BUILD_TARGET/usr/share/icons/hicolor/256x256@2/apps"
-cp "$DIR/../schema/org.x.sunrise-siren-studio.credentials.gschema.xml" "$BUILD_TARGET/usr/share/glib-2.0/schemas"
-cp "$DIR/../man/sunrise-siren-studio.1.gz" "$BUILD_TARGET/usr/share/man/man1"
+cp -v "$DIR/../build/sunrise-siren-studio" "$BUILD_TARGET/usr/bin"
+cp -v "$DIR/../dist/sunrise-siren-studio.desktop" "$BUILD_TARGET/usr/share/applications"
+cp -v "$DIR/../dist/sunrise-siren-studio.png" "$BUILD_TARGET/usr/share/icons/hicolor/256x256@2/apps"
+cp -v "$DIR/../schema/org.x.sunrise-siren-studio.credentials.gschema.xml" "$BUILD_TARGET/usr/share/glib-2.0/schemas"
+cp -v "$DIR/../man/sunrise-siren-studio.1.gz" "$BUILD_TARGET/usr/share/man/man1"
 
 echo "Package: sunrise-siren-studio
 Version: $STUDIO_VERSION
