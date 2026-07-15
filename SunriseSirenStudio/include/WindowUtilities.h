c788abb6c9971350a739abc271a799108817dac9
ngkon6 <nathankoning@gmail.com>
2026-07-15 20:23:46 +0200

    Add Ctrl+Q shortcut to quit

--- SunriseSirenStudio/include/WindowUtilities.h
+++ SunriseSirenStudio/include/WindowUtilities.h
@@ -520,7 +520,7 @@ static void main_window_key_press(GtkWidget *widget, GdkEventKey *e, gpointer da
     else if (e->keyval == 65307) go_to(widget, "MainMenu"); // Esc
     else if (e->keyval == 82 && ctrl_pressed) reboot_program(); // R
     else if (e->keyval == 114 && ctrl_pressed) reboot_clock(); // r
-    else if (e->keyval == 119 && ctrl_pressed) quit(); // w
+    else if ((e->keyval == 113 || e->keyval == 119) && ctrl_pressed) quit(); // q, w
     else if (e->keyval == 115 && ctrl_pressed) { // s
         universal_confirm(widget, data);
         ctrl_pressed = FALSE;
